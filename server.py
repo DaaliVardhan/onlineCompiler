df095f279eb84683f7975fd25ddda7bda5062a35
Daali <daalivardhan55@gmail.com>
2022-09-06 22:22:15 +0530

    initial commit

---
+++ server.py
@@ -0,0 +1,39 @@
+from flask import Flask,render_template,request,redirect,url_for
+import requests
+import json as j
+
+
+app=Flask(__name__)
+
+@app.route("/",methods=['POST','GET'])
+def get():
+    if request.method=="POST":
+        data=request.form
+        
+        lang={
+            "python":"python3",
+            "java":"java",
+            "c_cpp":"cpp"
+        }
+
+        body={"script": data["script"],
+        "stdin": data["stdin"],
+        "language": lang[data["lang"]],
+        "versionIndex": "0",
+        "clientId": "b75ceb42f2d6440802d30b6bfb234d83",
+        "clientSecret": "21966293b7bb0019a2ccef63201b7cd98ce3408cdc9d23b84341af1a73739029"
+        }
+        
+        res=requests.post(url="https://api.jdoodle.com/v1/execute",headers={"Content-Type":"application/json"},data=j.dumps(body))
+        return res.json()
+        
+    return render_template("index.html")
+
+@app.route("/sample/<name>",methods=["GET"])
+def set(name):
+    if name=="python" or name=="java" or name=="c_cpp":
+        with open(f"app/{name}/{name}.txt","r") as file:
+            data=file.readlines()
+            data=("").join(data)
+            return data
+    return {}
